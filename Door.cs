ce224b5bd62144f547dd559c74eac39e09a37f3a
Rick-Robert <rickrrtr@gmail.com>
2024-12-05 17:32:04 -0300

    Merge Conflict Markers removed

--- Door.cs
+++ Door.cs
@@ -4,17 +4,12 @@ using System;
 public partial class Door : Area2D
 {
 	private bool isOpen = false;
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> 2ab65d44b8ccc6e0e72c908ef0d6b640982fc29b
 	// Called when the node enters the scene tree for the first time.
 	public void Toggle()
 	{
 		isOpen = !isOpen;
-
-<<<<<<< HEAD
+		
 		Sprite2D doorSprite = GetNode<Sprite2D>("Sprite2D");
 		if(isOpen)
 		{
@@ -23,10 +18,7 @@ public partial class Door : Area2D
 		{
 			doorSprite.FlipH = false;
 		}
-=======
-		Sprite doorSprite = GetNode<Sprite>("Sprite");
-		doorSprite.Texture = isOpen ? (Texture)GD.Load("res://porta_aberta.png") : (Texture)GD.Load("res://porta_fechada.png");
->>>>>>> 2ab65d44b8ccc6e0e72c908ef0d6b640982fc29b
+
 	}
 
 }
--- Lever.cs
+++ Lever.cs
@@ -5,7 +5,7 @@ public partial class Lever : Area2D
 {
 	
 	[Export] public NodePath DoorPath;
-<<<<<<< HEAD
+
 	private bool isOn = false;
 	private bool Pushable = false;
 	
@@ -27,26 +27,12 @@ public partial class Lever : Area2D
 		}
 
 	}
-=======
 
-	private bool isOn = false;
-
-	
-	public override void  _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
-	{
-		if (@event is InputEventMouseButton eventMouse && eventMouse.Pressed)
-		{
-			Toggle();
-		}
-	}
-
->>>>>>> 2ab65d44b8ccc6e0e72c908ef0d6b640982fc29b
 	private void Toggle()
 	{
 		isOn = !isOn;
 		
 		// Troca a textura da alavanca de acordo com o estado
-<<<<<<< HEAD
 		Sprite2D leverSprite = GetNode<Sprite2D>("Sprite2D");
 		if(isOn)
 		{
@@ -55,11 +41,7 @@ public partial class Lever : Area2D
 		{
 			leverSprite.FlipH = false;
 		}
-=======
-		Sprite leverSprite = GetNode<Sprite>("Sprite");
-		leverSprite.Texture = isOn ? (Texture)GD.Load("res://alavanca_on.png") : (Texture)GD.Load("res://alavanca_off.png");
 
->>>>>>> 2ab65d44b8ccc6e0e72c908ef0d6b640982fc29b
 		// Envia o sinal para a porta
 		Node door = GetNode(DoorPath);
 		if (door != null)
